5325c827fadb03a1f2a7d3b2e3db69ddbaaeaf56
DeBruhl <144726013+DeBruhl@users.noreply.github.com>
2025-04-22 14:09:36 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,8 +43,7 @@ time_to_insert = st.button('Submit Order')
 if time_to_insert:
     my_insert_stmt = """ insert into SMOOTHIES.PUBLIC.ORDERS(INGREDIENTS,name_on_order)
         values ('""" + ingredients_string + """','"""+name_on_order+"""');"""
-# break      
-session.sql(my_insert_stmt).collect()
+    session.sql(my_insert_stmt).collect()
 st.success('Your Smoothie is ordered, '+name_on_order+'!' , icon="✅")
 
 my_dataframe = session.table("SMOOTHIES.PUBLIC.ORDERS").select(col('INGREDIENTS'),col('NAME_ON_ORDER'),col('ORDER_FILLED'))
